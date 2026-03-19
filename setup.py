27426802ffe200a636208658fd31e81ff2c2e81a
joselovesai <josestudies9087@gmail.com>
2026-03-19 13:56:48 +0100

    Bump version to 2.0.0 and update project description

--- setup.py
+++ setup.py
@@ -2,13 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name="ayova",
-    version="1.0.0",
-    description="AYOVA - Secure encrypted message vault CLI with gradient aesthetics",
-    author="Ayogwokhai Jose Maria",
+    version="2.0.0",
+    description="AYOVA P2P - Device-based, trust-first secure messenger by Ayogwokhai Josemaria",
+    author="Ayogwokhai Josemaria",
     packages=find_packages(),
     install_requires=[
         "click>=8.0",
         "rich>=13.0",
+        "pynacl>=1.5",
+        "asyncssh>=2.10",
         "cryptography>=41.0",
     ],
     entry_points={
